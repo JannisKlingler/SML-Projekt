8ddae3fa17a8cc5e6116ab64af686201a4584bb8
Clemens <clemens.schaechter@live.com>
2020-11-19 17:11:10 +0100

    Forschleife im Encoder und Decoder geschrieben. Der Aufbau von Encoder und Decoder werden in einer Liste festgelegt.

--- training.py
+++ training.py
@@ -1,29 +1,23 @@
 import matplotlib.pyplot as plt
-#from keras import backend as K
 import numpy as np
 import tensorflow as tf
-#from tensorflow import keras
-#from tensorflow.keras import layers
 
 import models
 import lossfunctions
 
 
 latent_dim = 5
-trainingsepochen = 1
+trainingsepochen = 10
+encoder_struc = [784, 500, 500, latent_dim]
+decoder_struc = [latent_dim, 500, 500, 784]
 
 
 (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
-data_dim = 784
-
-
 x_train = np.where(x_train > 127.5, 1.0, 0.0).astype('float32')
-x_train = np.reshape(x_train, (len(x_train), data_dim))
+x_train = np.reshape(x_train, (len(x_train), len(x_train[1]) ** 2))
 
-
-encoder = models.VAE_Dense_Encoder(data_dim, latent_dim, [500], "tanh")
-
-decoder = models.Bernoulli_Dense_Decoder(data_dim, latent_dim, [500], "tanh")
+encoder = models.VAE_Dense_Encoder(encoder_struc, "tanh")
+decoder = models.Bernoulli_Dense_Decoder(decoder_struc, "tanh")
 
 vae = tf.keras.Model(encoder.inp, decoder(encoder(encoder.inp)[2]))
 
@@ -31,12 +25,9 @@ vae = tf.keras.Model(encoder.inp, decoder(encoder(encoder.inp)[2]))
 vae.add_loss(lossfunctions.Bernoulli_Loss(encoder, decoder))
 vae.compile(optimizer='adam')
 
-
-
 vae.fit(x_train, x_train,
         epochs=trainingsepochen,
         batch_size=100)
-
 
 decoded_imgs = vae.predict(x_train)
 
@@ -51,8 +42,6 @@ for i in np.random.randint(len(x_train), size=n):
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-
-
 
     ax = plt.subplot(2, n, k + 1 + n)
     plt.imshow(decoded_imgs[i].reshape(28, 28))
