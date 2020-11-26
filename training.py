37da06cccb381cf44d038128b906120e451055a8
Ben Deitmar <ben.deitmar@gmail.com>
2020-11-26 10:27:59 +0100

    training etwas modularer gemacht. man kann jetzt am anfang die Aufgabe festlegen z.B. mit job = 'MNIST'

--- training.py
+++ training.py
@@ -7,25 +7,32 @@ import lossfunctions
 
 tf.random.set_seed(0)
 
+#Aufgabe vorgeben. Mögliche eingaben: 'MNIST', 'rotatingMNIST'
+job = 'MNIST'
+
 # Hyperparameter. latent_dim = 10 und epochs = 100 liefert gute Ergebnisse
 latent_dim = 10
-epochs = 100
+epochs = 10
 encoder_struc = [784, 500, 500, latent_dim]
 decoder_struc = [latent_dim, 500, 500, 784]
 dropout = 0.1  # Auf 0 setzen, falls nicht gewünscht
 akt_fun = "tanh"
 
-(x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
-x_train = np.where(x_train > 127.5, 1.0, 0.0).astype('float32')
-x_train = np.reshape(x_train, (len(x_train), encoder_struc[0]))
+if job == 'MNIST' :
+    (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+    x_train = np.where(x_train > 127.5, 1.0, 0.0).astype('float32')
+    x_train = np.reshape(x_train, (len(x_train), encoder_struc[0]))
 
-encoder = models.VAE_Dense_Encoder(encoder_struc, dropout, akt_fun)
-decoder = models.Bernoulli_Dense_Decoder(decoder_struc, dropout, akt_fun)
+    encoder = models.VAE_Dense_Encoder(encoder_struc, dropout, akt_fun)
+    decoder = models.Bernoulli_Dense_Decoder(decoder_struc, dropout, akt_fun)
+    loss = lossfunctions.Bernoulli_Loss(encoder, decoder)
+else:
+    raise Exception("That job does not exist")
+
 
 vae = tf.keras.Model(encoder.inp, decoder(encoder(encoder.inp)[2]))
 
-
-vae.add_loss(lossfunctions.Bernoulli_Loss(encoder, decoder))
+vae.add_loss(loss)
 vae.compile(optimizer='adam')
 
 vae.fit(x_train, x_train,
@@ -39,19 +46,20 @@ rec_imgs = vae.predict(x_train)
 n = 20
 k = 0
 plt.figure(figsize=(20, 4))
-for i in np.random.randint(len(x_train), size=n):
-    ax = plt.subplot(2, n, k + 1)
-    plt.imshow(x_train[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+if job == 'MNIST':
+    for i in np.random.randint(len(x_train), size=n):
+        ax = plt.subplot(2, n, k + 1)
+        plt.imshow(x_train[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
-    ax = plt.subplot(2, n, k + 1 + n)
-    plt.imshow(rec_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    k = k + 1
+        ax = plt.subplot(2, n, k + 1 + n)
+        plt.imshow(rec_imgs[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        k = k + 1
 plt.show()
 
 # Bei Bedarf: Modell speichern und laden
