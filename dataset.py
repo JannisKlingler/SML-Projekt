4c97c4d4b686846eef12729c9a50263a38a3afbb
Clemens <clemens.schaechter@live.com>
2020-11-26 18:57:03 +0100

    Erzeugen von Trainings und Testdatensatz. Im Testdatensatz werden zufällig zwei Frames gelöscht.

--- dataset.py
+++ dataset.py
@@ -5,28 +5,35 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-frames = 20
-(x_train, y_train), _ = keras.datasets.mnist.load_data()
+frames = 10
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 
-def rot(pic, frames):
-    video = []
+x_train = list(map(lambda b: list(map(lambda i: np.where(sp.ndimage.rotate(
+    b, (i+1) * 360/frames, reshape=False) > 127.5, 1.0, 0.0).astype('float32'), range(frames))), x_train))
+
+x_test = list(map(lambda b: list(map(lambda i: np.where(sp.ndimage.rotate(
+    b, (i+1) * 360/frames, reshape=False) > 127.5, 1.0, 0.0).astype('float32'), range(frames))), x_test))
+
+
+for j in range(len(x_test)):
+    for i in np.random.randint(10, size=2):
+        x_test[j][i] = np.zeros((28, 28))
+
+train = np.array(x_train)
+test = np.array(x_test)
+
+np.save('rotatingMNIST_train', train)
+np.save('rotatingMNIST_test', test)
+
+#x_test = np.load('C:/Users/Admin/Desktop/Python/rotatingMNIST_test.npy')
+
+
+plt.figure(figsize=(20, 20))
+for j in range(10):
     for i in range(frames):
-        im = sp.ndimage.rotate(pic, i*360/frames, reshape=False)
-        video.append([im])
-    return video
-
-
-def dataset(size, frames):
-    data = []
-    for i in range(size):
-        vid = rot(x_train[i], frames)
-        data.append([vid])
-    return data
-
-
-data = dataset(1, 10)
-print(len(data))
-plt.figure(figsize=(20, 4))
-plt.imshow(data[0, 1, 0, :, :])
+        ax = plt.subplot(10, frames, j*10+(i+1))
+        plt.imshow(test[j][i], cmap='gray')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 plt.show()
