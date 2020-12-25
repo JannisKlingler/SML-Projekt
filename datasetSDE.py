4171dae30b5f434f6feb729817aa2788e6dbd17c
Ben Deitmar <ben.deitmar@gmail.com>
2020-12-25 23:06:58 +0100

    zu datasetSDE die möglichkeit hinzugefügt graphen zu glätten

--- datasetSDE.py
+++ datasetSDE.py
@@ -3,49 +3,66 @@ import scipy as sp
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from math import exp, pi
 
 
 from mpl_toolkits.axes_grid1 import ImageGrid
 from math import sin, cos, sqrt, pi, floor, ceil, exp, log
 from random import choice, uniform
 
-# T = Zeit, die simuliert wird
-# D = Anzahl der simulierten schritte pro Sekunde
-#X_0 = Startwert
-def SDE(T,D,X_0,mu_sigma):
-    mu, sigma = mu_sigma
-    N = ceil(T*D)
-    E = sqrt(3/D)
-    Y = np.random.uniform(-E,E,N)
-    #Y = list(map(lambda i: uniform(-E,E), range(0,N)))
-    X = [X_0]
-    for i in range(0,N):
-        X.append( X[-1] + mu(i/D, X[-1])/D + sigma(i/D, X[-1]) * Y[i])
-    return X
-
 
 frames = 100
 T = 1
+fps = frames//T
 
-SDE_List = [(lambda t,x : t, lambda t,x : 0), (lambda t,x : 0, lambda t,x : 1)]
+SDE_List = [(lambda t,x : 1, lambda t,x : t)]
 
 #Datensatzgröße
 Ntrain = 5000
 Ntest = 1000
 
 
-x_train = list(map(lambda i : SDE(T, frames/T, 1, choice(SDE_List)), range(Ntrain)))
-x_test = list(map(lambda i : SDE(T, frames/T, 1, choice(SDE_List)), range(Ntest)))
+
+# T = Zeit, die simuliert wird
+# D = Anzahl der simulierten schritte pro Sekunde
+#X_0 = Startwert
+def SDE(T,D,X_0,mu_sigma):
+    mu, sigma = mu_sigma
+    N = ceil((T+1)*D)
+    E = sqrt(3/D)
+    Y = np.random.uniform(-E,E,N)
+    #Y = list(map(lambda i: uniform(-E,E), range(0,N)))
+    X = list(map(lambda arg: X_0,range(fps)))
+    for i in range(0,N):
+        X.append( X[-1] + mu(i/D, X[-1])/D + sigma(i/D, X[-1]) * Y[i])
+    return X
+
+K_rbf = lambda y : exp(-y**2)/sqrt(pi)
+z = 0.5     #Zwischen 1/fps und 1
+K_historyAverage = lambda y : 1/z if (y > -z and y <= 0) else 0
+
+def smooth(X, h, K):
+    Xnew = []
+    for i in range(len(X)):
+        Xnew.append(sum(map(lambda j: (1/fps)*X[j]*K((i-j)/fps/h)/h, range(len(X)))))
+    return Xnew
+
+
+
+x_train = list(map(lambda i : SDE(T, fps, 1, choice(SDE_List)), range(Ntrain)))
+x_test = list(map(lambda i : SDE(T, fps, 1, choice(SDE_List)), range(Ntest)))
 
 
 
 ##########################
 #Zeichen
-fig, axs = plt.subplots(5, 5)
+fig, axs = plt.subplots(6, 6)
 
-for i in range(25):
-    j,k = i//5 , i%5
-    axs[j, k].plot(list(np.linspace(0,T,frames+1)),x_train[i])
-    axs[j, k].axis((0,T,-1,3))
+for i in range(18):
+    j,k = i//6 , i%6
+    axs[2*j, k].plot(list(np.linspace(0,T,frames)),x_train[i][fps:(T+1)*fps])
+    axs[2*j, k].axis((0,T,-1,3))
+    axs[2*j+1, k].plot(list(np.linspace(0,T,frames)),smooth(x_train[i], 0.2, K_historyAverage)[fps:(T+1)*fps])
+    axs[2*j+1, k].axis((0,T,-1,3))
 #plt.draw()
 plt.show()
